77ab73ec63d8e48dd1f4166f9360aacdab7dedcf
J. Vitor <srvitorcastro@gmail.com>
2023-06-09 09:17:50 -0300

    Finalizando cenários de tasks

--- projects/mark85-cypress-api/cypress.config.js
+++ projects/mark85-cypress-api/cypress.config.js
@@ -12,6 +12,13 @@ module.exports = defineConfig({
           const users = db.collection('users')
           await users.deleteMany({ email: email })
           return null
+        },
+        async deleteTask(taskName, emailUser) {
+          const users = db.collection('users')
+          const user = users.findOne({ email: emailUser })
+          const tasks = db.collection('tasks')
+          await tasks.deleteMany({ name: user._id })
+          return null
         }
       })
     },
