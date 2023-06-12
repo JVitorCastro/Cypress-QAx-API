348b1b21e983fbf654007ec730daf3f6cab07aa2
J. Vitor <srvitorcastro@gmail.com>
2023-06-12 11:19:22 -0300

    Configurando variáveis de ambiente com dotenv

--- projects/mark85-cypress-api/cypress.config.js
+++ projects/mark85-cypress-api/cypress.config.js
@@ -1,6 +1,7 @@
-const { defineConfig } = require("cypress");
-
+const { defineConfig } = require("cypress")
 const { connect } = require('./cypress/support/mongo')
+
+require('dotenv').config()
 
 module.exports = defineConfig({
   e2e: {
@@ -27,13 +28,13 @@ module.exports = defineConfig({
         }
       })
     },
-    baseUrl: 'http://localhost:3333',
+    baseUrl: process.env.BASE_URL,
     video: false,
     screenshotOnRunFailure: false,
     env:{
-      amqpHost: 'https://shark.rmq.cloudamqp.com/api/queues/kkrxotnc',
-      amqpQueue: 'tasks',
-      amqpToken: 'Basic a2tyeG90bmM6ZlFPRnlnNHRIT0FoRm4tYnc2TjgydlI4RHFHY1p5Qzc='
+      amqpHost: process.env.AMQP_HOST,
+      amqpQueue: process.env.AMQP_QUEUE,
+      amqpToken: process.env.AMQP_TOKEN
     }
   },
 });
